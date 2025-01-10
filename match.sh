cf2423b32c1b476548b1867d5b19264c7e5acd8b
Alexander <lebo8020@mylaurier.ca>
2025-01-10 09:33:56 -0500

    added use and awk

--- match.sh
+++ match.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Return timestamp of input line, sourced from movie and tv subtitles
+# Usage: $ match.sh "line" "media name" "is-movie" (bool)
+
 quote="$1"
 name="$2"
 movie=$3
 
 if [[ $movie -eq 1 ]]
 then
-	rg "$quote" ./movies/"$name"/*.srt | head -1
+	term="$(rg -C 3 "$quote" ./movies/"$name"/*.srt | head | grep -e "-->")"
 else
-	rg "$quote" ./series/"$name"/*.srt | head -1
+	term="$(rg -C 3 "$quote" ./series/"$name"/*.srt | head | grep -e "-->")"
 fi
+
+awk -F , '{print $1}' <<< "${term}"
